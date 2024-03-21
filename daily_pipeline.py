40901ee688f39caec6ab1ab8fe20cf7c40280a89
caspercrause <ccrause07@gmail.com>
2024-03-21 21:10:30 +0200

    feat: Fixed where clause in `daily_pipeline.py`

--- daily_pipeline.py
+++ daily_pipeline.py
@@ -6,7 +6,7 @@ from datetime import datetime as dt
 from dateutil.relativedelta import relativedelta
 
 # Create a string based on the date of today and floor it to the beginning of the previous month
-CurrentMonth = (dt.now() - relativedelta(months=1)).strftime('%Y-%m-01T00:00:00.000')
+CurrentMonth = (dt.now() - relativedelta(months=1)).strftime('%Y-%m-01T00:01:00.000')
 
 # Unauthenticated client only works with public data sets. Note 'None'
 # in place of application token, and no username or password:
@@ -14,7 +14,7 @@ client = Socrata("data.sfgov.org", None)
 dataset_id = "wg3w-h783"
 
 # Filter data set for all records in the current month
-where_clause = f'report_datetime > "{CurrentMonth}"'
+where_clause = f"report_datetime > '{CurrentMonth}'"
 generator = client.get_all(dataset_id, where = where_clause)
 
 pipeline = dlt.pipeline(
